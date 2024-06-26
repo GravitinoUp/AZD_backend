9f762d58b83c332e4620b7bc44065310c7ebce9a
DevTriangle <halbert370@gmail.com>
2024-06-26 14:32:53 +0300

    Исправлена ошибка

--- src/modules/purchase/purchase.module.ts
+++ src/modules/purchase/purchase.module.ts
@@ -7,6 +7,7 @@ import { UserModule } from '../user/user.module'
 import { PurchaseTypeModule } from '../purchase-type/purchase-type.module'
 import { OrganizationModule } from '../organization/organization.module'
 import { RolePermissionModule } from '../role-permission/role-permission.module'
+import { CurrencyModule } from '../currency/currency.module'
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { RolePermissionModule } from '../role-permission/role-permission.module'
     PurchaseTypeModule,
     OrganizationModule,
     RolePermissionModule,
+    CurrencyModule,
   ],
   controllers: [PurchaseController],
   providers: [PurchaseService],
