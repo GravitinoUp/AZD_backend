5ead1229074315a42cd18ac923ae567ebc568c52
DevTriangle <halbert370@gmail.com>
2024-07-01 12:40:08 +0300

    Исправлены ошибки миграций

--- src/db/migrations/1719409730142-UpdateKosgu.ts
+++ src/db/migrations/1719409730142-UpdateKosgu.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
 
-export class UpdateKosgu1719412831577 implements MigrationInterface {
+export class UpdateKosgu1719409730142 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('Kosgu', 'kosgu_name')
   }
